77aa001d5898235fb430a27a333b42e650c9e109
snowcone999 <30340064+snowcone999@users.noreply.github.com>
2020-12-07 18:19:44 +0900

    Update and rename smug.txt to smug.js

--- smug.js
+++ smug.js
@@ -12,7 +12,7 @@ module.exports = {
 
     const embed = new MessageEmbed()
       .setFooter(message.author.username)
-      .setColor("BLUE")
+      .setColor("YELLOW")
       .setDescription(`[Click here if the image failed to load.](${data.url})`)
       .setImage(`${data.url}`)
       .setTimestamp();
